44ad6fafeac4125137de59aa0a1b05a940edd3b6
laur1290 <laur1290@ISS2TVRKH2.scdsb.pri>
2019-09-12 11:43:42 -0400

    Revert "Unit review"
    This reverts commit d6dc927abf5eaf359fecec5456e08b66b99ed9cc.

--- src/Lesson3/MathClass.java
+++ src/Lesson3/MathClass.java
@@ -9,7 +9,7 @@ public class MathClass {
         System.out.println(Math.sqrt(25)); // root of 25
         double y = 8.9876;
         System.out.println(Math.round(y));
-        System.out.println("The bigger number is " + Math.max(x, y)); //max=heighest
+        System.out.println("The bigger number is " + Math.max(x, y)); //max = heighest
     }
     
 }
